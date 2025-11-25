9b0207ff0561879d91d11602bc20296122d76230
NataliyaKh <wastelin@mail.ru>
2025-11-25 23:12:25 +0300

    interrupt

--- prod/main.tf
+++ prod/main.tf
@@ -1,40 +1,41 @@
 # StorageClass
-resource "kubernetes_storage_class_v1" "cluster_storage" {
+data "kubernetes_storage_class_v1" "existing_standard" {
   metadata {
     name = "standard"
   }
-  
+}
+
+locals {
+  storage_class_exists = can(data.kubernetes_storage_class_v1.existing_standard.metadata[0].name)
+}
+
+resource "kubernetes_storage_class_v1" "cluster_storage" {
+  count = local.storage_class_exists ? 0 : 1
+
+  metadata {
+    name = "standard"
+  }
+
   storage_provisioner = "kubernetes.io/no-provisioner"
   volume_binding_mode = "WaitForFirstConsumer"
   reclaim_policy      = "Retain"
 }
 
-
 module "kube_prometheus" {
   source = "../modules/kube-prometheus"
-
 
   # Basic parameters
   cluster_name     = var.cluster_name
   environment      = "prod"
-  
+
   # Versions
   prometheus_stack_version = var.prometheus_stack_version
-  
-  # Resources
-  prometheus_replicas = var.prometheus_replicas
-  prometheus_retention = var.prometheus_retention
-  
-  # Yandex Cloud
+
+  # Storage class for PVC in Prometheus stack
   storage_class_name = "standard"
-  
-  # Secrets
-  grafana_admin_password = var.grafana_admin_password
-  
-  # Monitoring
-  enable_alertmanager = true
-  enable_thanos       = var.enable_thanos
-  
-  # Helm values
-  extra_values = var.extra_values
+}
+
+# Output
+output "storage_class_info" {
+  value = local.storage_class_exists ? "StorageClass 'standard' уже существует в кластере" : "StorageClass 'standard' будет создан"
 }
