b0079b14977b9964d9d1b22be174f7c3a43da8b3
NataliyaKh <wastelin@mail.ru>
2025-11-25 23:25:39 +0300

    tfvars

--- prod/main.tf
+++ prod/main.tf
@@ -39,3 +39,51 @@ module "kube_prometheus" {
 output "storage_class_info" {
   value = local.storage_class_exists ? "StorageClass 'standard' уже существует в кластере" : "StorageClass 'standard' будет создан"
 }
+
+Ruslan Machnev, [25.11.2025 23:10]
+prod/main.tf
+
+# StorageClass с проверкой существования
+data "kubernetes_storage_class_v1" "existing_standard" {
+  metadata {
+    name = "standard"
+  }
+}
+
+locals {
+  storage_class_exists = can(data.kubernetes_storage_class_v1.existing_standard.metadata[0].name)
+}
+
+resource "kubernetes_storage_class_v1" "cluster_storage" {
+  count = local.storage_class_exists ? 0 : 1
+
+  metadata {
+    name = "standard"
+  }
+
+  storage_provisioner = "kubernetes.io/no-provisioner"
+  volume_binding_mode = "WaitForFirstConsumer"
+  reclaim_policy      = "Retain"
+}
+
+module "kube_prometheus" {
+  source = "../modules/kube-prometheus"
+
+  # Basic parameters
+  cluster_name     = var.cluster_name
+  environment      = "prod"
+
+  # Versions
+  prometheus_stack_version = var.prometheus_stack_version
+
+  # Storage class для PVC в Prometheus stack
+  storage_class_name = "standard"
+}
+
+# Output для информации
+output "storage_class_info" {
+  value = local.storage_class_exists ? "StorageClass 'standard' уже существует в кластере" : "StorageClass 'standard' будет создан"
+}
+
+# Grafana authentication
+grafana_admin_password = var.grafana_admin_password
